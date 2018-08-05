a54e130144f59caf02520e5c92a524bb88b6806d
dipikabaad <dipika.baad@gmail.com>
2018-08-05 13:36:09 +0300

    New changes to the repository

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import Vue from 'vue';
+import Vue from 'vue'
 import VueRouter from 'vue-router';
-import App from './App.vue';
-import {routes} from './routes'
+import App from './App.vue'
+import { routes } from './routes';
 
 Vue.use(VueRouter);
 
@@ -25,5 +25,5 @@ export const eventBus = new Vue();
 new Vue({
     el: '#app',
     render: h => h(App),
-    router: router 
+    router: router
 })
