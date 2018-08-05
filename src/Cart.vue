7225c3d2fd96b161a1d0c4fb90d411c94161d7bb
dipikabaad <dipika.baad@gmail.com>
2018-08-05 23:31:26 +0300

    Component Guards

--- src/Cart.vue
+++ src/Cart.vue
@@ -62,6 +62,14 @@
 
     export default {
         props: ['cart'],
-        mixins: [ CartMixin ]
+        mixins: [ CartMixin ],
+        beforeRouteLeave(to, from, next){
+            if(this.cart.items.length > 0){
+                if(!confirm('Are you sure that you don\'t want to buy these awesome products')){
+                    return next(false);
+                }
+            }
+            next();
+        }
     }
 </script>
