55389cd6ed51799fc8018cd5faae4630f11da7e7
dipikabaad <dipika.baad@gmail.com>
2018-08-05 15:23:13 +0300

    Aliases

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ export const routes = [
 	//LEGACY ROUTES
 
     { path: '', component: ProductList },
-    { path: '/cart', component: Cart },
+    { path: '/cart', component: Cart, alias: '/shopping-cart' },
     {path: '/products/:productId',props:true, name:'viewProduct', component: ViewProduct}, //question mark means without id also it works
-    { path: '*', component: { template: '<h1>Page Not Found!</h1>' } }
+    {path: '*', component: { template: '<h1>Page Not Found!</h1>' } }
 ];
